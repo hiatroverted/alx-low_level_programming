6118a775d4fe860fc51cb225c0d74f1e7e86bcad
hiatrovered <audiobants@gmail.com>
2023-07-13 08:04:15 -0700

    print various integer size types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - program prints the size of various computer types
+ * main - program prints the size of various types of integers
  * Return: 0 (Successful)
  */
 int main(void)
@@ -8,13 +8,13 @@ int main(void)
 char c;
 int i;
 long int l;
-long long int m;
+long long int a;
 float f;
 
-printf("Size of a char: %c byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %i byte(s)\n", (unsigned int)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
-printf("Size of a long long int: %llu byte(s)\n", (unsigned long long)sizeof(m));
-printf("Size of a float: %f byte(s)\n", (float)sizeof(f));
+printf("Size of a long long int: %llu byte(s)\n", (unsigned long long)sizeof(a));
+printf("Size of a float: %lu byte(s)\n", (float)sizeof(f));
 return (0);
 }
