463aa6f22ec15865fc83f3fab3488d2001d4c8e6
hiatrovered <audiobants@gmail.com>
2023-07-14 11:08:56 -0700

    fixing errors

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,11 +3,11 @@
  * main - print alphabet in lowercase
  * Return: 0
  */
-int main(void)
+char main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; ch++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
